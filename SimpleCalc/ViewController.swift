4fab61f2b47d2530adc5f98007b081291e6b0c88
bill0930 <bill0930@gmail.com>
2019-10-12 03:51:51 +0800

    first commit

---
+++ SimpleCalc/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SimpleCalc
+//
+//  Created by CHAN CHI YU on 12/10/2019.
+//  Copyright © 2019 Billy  Chan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
